270906bda7d93cfe71323041bee159f97ffbec25
Suskyn <75987968+Suskyn@users.noreply.github.com>
2021-09-13 15:21:18 +0200

    Validacie1

--- uu_tatranci_maing01-server/app/api/validation_types/article-types.js
+++ uu_tatranci_maing01-server/app/api/validation_types/article-types.js
@@ -1,1 +1,26 @@
 /* eslint-disable */
+const createArticleDtoInType = shape({
+  name : string(200).isRequired(),
+  desc: uu5String(16000).isRequired(),
+  content: uu5String(16000).isRequired(),
+  titleImg: uri(),
+  autorsUuid: uuIdentity().isRequired(),
+  autorsName: string()
+});
+const getArticleDtoInType = shape({
+  id: id().isRequired()
+});
+const listArticleDtoInType = shape({
+
+});
+const deleteArticleDtoInType = shape({
+  id: id().isRequired()
+});
+const updateArticleDtoInType = shape({
+  id: id(),
+  desc: uu5String(16000),
+  content: uu5String(16000),
+  autorsUuid: uuIdentity(),
+  titleImg: uri(),
+  autorsName: string()
+});
