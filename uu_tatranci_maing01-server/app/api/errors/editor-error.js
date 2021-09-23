903424a5e935b27ff762369cddb29cc33674c259
matus124 <75988476+matus124@users.noreply.github.com>
2021-09-23 15:02:59 +0200

    editor

--- uu_tatranci_maing01-server/app/api/errors/editor-error.js
+++ uu_tatranci_maing01-server/app/api/errors/editor-error.js
@@ -5,27 +5,41 @@ const EDITOR_ERROR_PREFIX = `${TatranciMainUseCaseError.ERROR_PREFIX}editor/`;
 
 const Create = {
   UC_CODE: `${EDITOR_ERROR_PREFIX}create/`,
-  
+
+  InvalidDtoIn: class extends TatranciMainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.code = `${Create.UC_CODE}invalidDtoIn`;
+      this.message = "DtoIn is not valid.";
+    }
+  },
 };
 
 const Get = {
   UC_CODE: `${EDITOR_ERROR_PREFIX}get/`,
-  
+
+  InvalidDtoIn: class extends TatranciMainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.code = `${Get.UC_CODE}invalidDtoIn`;
+      this.message = "DtoIn is not valid.";
+    }
+  },
 };
 
 const List = {
   UC_CODE: `${EDITOR_ERROR_PREFIX}list/`,
-  
+
 };
 
 const Delete = {
   UC_CODE: `${EDITOR_ERROR_PREFIX}delete/`,
-  
+
 };
 
 const Update = {
   UC_CODE: `${EDITOR_ERROR_PREFIX}update/`,
-  
+
 };
 
 module.exports = {
