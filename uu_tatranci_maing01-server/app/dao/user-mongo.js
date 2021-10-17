d86fbd8846e51c8b396dae1bda2fc4d1b3fe0de4
Suskyn <75987968+Suskyn@users.noreply.github.com>
2021-10-17 19:11:59 +0200

    Index fix

--- uu_tatranci_maing01-server/app/dao/user-mongo.js
+++ uu_tatranci_maing01-server/app/dao/user-mongo.js
@@ -4,7 +4,7 @@ const { UuObjectDao } = require("uu_appg01_server").ObjectStore;
 class UserMongo extends UuObjectDao {
 
   async createSchema(){
-    await super.createIndex({ awid: 1, id: 1 }, { unique: true });
+    await super.createIndex({ awid: 1, _id: 1 }, { unique: true });
     await super.createIndex({ awid: 1, uuId: 1 }, { unique: true });
   }
   async create(uuObject) {
